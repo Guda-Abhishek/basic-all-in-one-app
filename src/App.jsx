33c4aeaa0a8d4e458612afc523a48ef6d54eaf44
Abhishek <abhishek.guda123@gmail.com>
2025-05-16 18:02:37 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer  from './components/Footer'
+import Navbar from './components/Navbar'
 import Dashboard from './components/modules/Dashboard'
 import AgriPage from './components/modules/Agripage'
 import HomeSecurityPage from './components/modules/HomeSecurityPage'
@@ -12,6 +13,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => (
   <Router>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/agri" element={<AgriPage />} />
